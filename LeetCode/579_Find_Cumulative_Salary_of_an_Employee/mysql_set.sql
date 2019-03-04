dff673e7aa9d11fab816a7670b9bbbe91c8a386a
Shaw <shawlu@github.com>
2019-03-03 22:10:33 -0800

    Fied indentation.

--- LeetCode/579_Find_Cumulative_Salary_of_an_Employee/mysql_set.sql
+++ LeetCode/579_Find_Cumulative_Salary_of_an_Employee/mysql_set.sql
@@ -1,15 +1,15 @@
 -- MySQL: set solution
 SELECT
-	e.Id
-	,e.Month
-	,IFNULL(e.Salary, 0) + IFNULL(e1.Salary, 0) + IFNULL(e2.Salary, 0) AS Salary
+  e.Id
+  ,e.Month
+  ,IFNULL(e.Salary, 0) + IFNULL(e1.Salary, 0) + IFNULL(e2.Salary, 0) AS Salary
 FROM Employee e
 LEFT JOIN Employee e1 ON e.Id = e1.Id AND e.Month = e1.Month + 1
 LEFT JOIN Employee e2 ON e.Id = e2.Id AND e.Month = e2.Month + 2
 WHERE (e.Id, e.Month) NOT IN (
-	SELECT 
-		Id
-		,MAX(Month) AS max_month
-	FROM Employee 
-	GROUP BY Id)
+  SELECT 
+    Id
+    ,MAX(Month) AS max_month
+  FROM Employee 
+  GROUP BY Id)
 ORDER BY e.Id ASC, e.Month DESC;
