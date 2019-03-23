1834f73cc0bb96613358d6f3795c79580fb98be4
Shaw <shawlu@github.com>
2019-03-23 16:41:37 -0700

    Renamed problem.

--- Interview/14_Facebook_Common_Friend/solution.sql
+++ Interview/14_Facebook_Common_Friend/solution.sql
@@ -4,24 +4,6 @@ UNION ALL
 SELECT friend_id, user_id FROM Friendship
 )
 SELECT
-	ab.user_id AS a
-	,ab.friend_id AS b
-	,af.friend_id AS a_friend
-	,bf.friend_id AS b_friend
-FROM tmp AS ab
-JOIN tmp AS af
-	ON ab.user_id = af.user_id
-JOIN tmp AS bf
-	ON ab.friend_id = bf.user_id
-	AND bf.friend_id = af.friend_id
-ORDER BY a, b, a_friend, b_friend;
-
-WITH tmp AS (
-SELECT user_id, friend_id FROM Friendship
-UNION ALL
-SELECT friend_id, user_id FROM Friendship
-)
-SELECT
 	ab.user_id
 	,ab.friend_id
 	,COUNT(*) AS common_friend
@@ -32,4 +14,5 @@ JOIN tmp AS bf
 	ON ab.friend_id = bf.user_id
 	AND bf.friend_id = af.friend_id
 GROUP BY ab.user_id, ab.friend_id
+HAVING common_friend >= 3
 ORDER BY common_friend DESC;
