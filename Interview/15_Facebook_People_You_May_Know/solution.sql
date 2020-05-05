00cde5ded3ab6542bdbc9a1f7c330c43425027ea
Jerry Wu <wctjerry@gmail.com>
2020-05-05 11:27:36 +0800

    Fix a bug that may miss user_id due to one-way friendship binding.

--- Interview/15_Facebook_People_You_May_Know/solution.sql
+++ Interview/15_Facebook_People_You_May_Know/solution.sql
@@ -3,14 +3,15 @@ SELECT user_id, friend_id FROM Friendship
 UNION ALL
 SELECT friend_id, user_id FROM Friendship
 )
+
 SELECT 
 	a.user_id
 	,b.user_id
 	,COUNT(*) AS common_friend
 FROM
-(SELECT DISTINCT user_id FROM Friendship) AS a
+(SELECT DISTINCT user_id FROM tmp) AS a
 CROSS JOIN
-(SELECT DISTINCT user_id FROM Friendship) AS b
+(SELECT DISTINCT user_id FROM tmp) AS b
 	ON a.user_id != b.user_id
 	AND (a.user_id, b.user_id) NOT IN (SELECT user_id, friend_id FROM tmp)
 JOIN tmp AS af
@@ -19,4 +20,5 @@ JOIN tmp AS bf
 	ON b.user_id = bf.user_id
 	AND bf.friend_id = af.friend_id
 GROUP BY a.user_id, b.user_id
+HAVING COUNT(*) >= 3
 ORDER BY common_friend DESC;
